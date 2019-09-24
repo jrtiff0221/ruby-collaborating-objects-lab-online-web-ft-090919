38d5378c69c41be1f578806beb31433b7555d8d4
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-24 17:58:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,11 +22,12 @@ attr_accessor :artist, :name
     song = Song.new(song_name)
 
     song.artist_name = artist_name
+    new_song = Song.new(song_name)
+    new_song.artist_name = artist_name
+    new_song
   end
   
-  def name
-    @name
-  end
+
 
   def artist_name=(name)
     @artist = Artist.find_or_create_by_name(name)
