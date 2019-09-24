63d04be9b4239502ab584c3588ea93e84a1cd2d1
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-24 16:43:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,11 +21,11 @@ attr_accessor :name
   end
  
   def songs
-    Song.all.select {|song| song.artist == self}
+    Song.all.select { |song| song.artist == self }
   end
   
   def self.find_or_create_by_name(name)
-    unless @@all.find {|artist| artist.name = name}
+    unless @@all.find { |artist| artist.name = name }
       Artist.new(name)
     else
       @@all.find {|artist|artist.name = name}
