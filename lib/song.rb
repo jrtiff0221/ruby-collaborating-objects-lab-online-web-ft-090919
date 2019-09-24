a87e5cb07d54b3d228c370886b4a3c39406b5cb0
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-24 17:52:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ attr_accessor :artist, :name
   
   
   def self.new_by_filename(file)
-    artist_name, song_name = file.split("-")
+    artist_name, song_name = file.split("-")[..1]
     song = Song.new(song_name)
     song.artist_name = artist_name
   end
