3b84eae3e80165af95e0ad5810bdab15558df28d
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-24 17:39:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,7 @@ attr_accessor :artist, :name
   def self.new_by_filename(file)
     artist_name, song_name = file.split("-")
     puts song_name
-    song = Song.new(song_name)
-    puts song.name
+    song = new(song_name)
     song.artist = Artist.find_or_create_by_name(artist_name)
   end
     
