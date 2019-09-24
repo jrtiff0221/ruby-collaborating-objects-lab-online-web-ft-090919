10a5720d2273ce65faf04144b6e1d4fa4cbd0bd7
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-24 18:10:47 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,12 +7,7 @@ class MP3Importer
   end
     
   def files
-    dir_files = Dir[path + "/*mp3"]
-     mp3_files = []
-     dir_files.each do |file|
-       mp3_files << file.split("/").last 
-      end
-      mp3_files
+   
     
   
 end
