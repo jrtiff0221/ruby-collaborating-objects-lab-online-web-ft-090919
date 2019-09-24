c1295b55357efa3c6d58ba025ba3d6f4db720dbb
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-24 17:10:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,13 @@ attr_accessor :artist, :name
   end
     
    
-  
+   def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
 
   
 end
